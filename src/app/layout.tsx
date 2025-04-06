a8d1bdac5919cb69e004b8629f89cc6fd22bb4c3
r-mccarty <ry.mccarty13@gmail.com>
2025-04-06 13:58:42 -0700

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,95 @@
+import type { Metadata } from "next"
+import localFont from "next/font/local"
+import "./globals.css"
+
+import Footer from "@/components/ui/Footer"
+import { NavBar } from "@/components/ui/Navbar"
+import { siteConfig } from "./siteConfig"
+
+// Define Barlow font
+const barlowFont = localFont({
+  src: [
+    {
+      path: "../../public/fonts/barlow-latin-400-normal-7fa387951673abf164b13dd1b45c70e3.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/barlow-latin-500-normal-50adbbfa3bfe480bf4246ff5bad7ad06.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/barlow-latin-700-normal-dd5b2912dbf896310865c1e9ac85ab41.woff2",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+  variable: "--font-barlow",
+  display: "swap",
+})
+
+// Define Colfax font
+const colfaxFont = localFont({
+  src: [
+    {
+      path: "../../public/fonts/ColfaxWebRegular-ffe8279204a8eb350c1a8320336a8e1a.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/ColfaxWebMedium-5cd963f45f4bd8647a4e41a58ca9c4d3.woff2",
+      weight: "500",
+      style: "normal",
+    },
+  ],
+  variable: "--font-colfax",
+  display: "swap",
+})
+
+export const metadata: Metadata = {
+  metadataBase: new URL("https://yoururl.com"),
+  title: siteConfig.name,
+  description: siteConfig.description,
+  keywords: ["Marketing", "Database", "Software"],
+  authors: [
+    {
+      name: "yourname",
+      url: "",
+    },
+  ],
+  creator: "yourname",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: siteConfig.url,
+    title: siteConfig.name,
+    description: siteConfig.description,
+    siteName: siteConfig.name,
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: siteConfig.name,
+    description: siteConfig.description,
+    creator: "@yourname",
+  },
+  icons: {
+    icon: "/favicon.ico",
+  },
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en" className={`${barlowFont.variable} ${colfaxFont.variable}`}>
+      <body className="min-h-screen overflow-x-hidden scroll-auto bg-gray-50 antialiased selection:bg-orange-100 selection:text-orange-600">
+        <NavBar />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  )
+}
